3ddd56835b8dffe3927ace5f9fca191ee5095d84
aratahorie <a.horie@commissure.co.jp>
2025-10-16 10:02:29 +0900

    Update static build with relative base

--- assets/download-CXr9582T.js
+++ assets/download-CXr9582T.js
@@ -1,4 +1,4 @@
-import{g as kn,R as Wt,r as ht,j as v,D as An,d as On}from"./index-CxtKjxSR.js";/*!
+import{g as kn,R as Wt,r as ht,j as v,D as An,d as On}from"./index-CPgOvZNa.js";/*!
  * Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
