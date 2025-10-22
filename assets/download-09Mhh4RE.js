6013314a782d47a5198437ed8b3f693d347c5e38
aratahorie <a.horie@commissure.co.jp>
2025-10-23 01:49:05 +0900

    Deploy: Add theme toggle and Layer-Tap highlighting
    - Add theme toggle button with light/dark mode switching
    - Add Layer-Tap key highlighting with orange color
    - Use softer orange color in dark mode for better contrast
    - Theme preference persists across sessions
    - Clean up old dist and src directories from gh-pages

--- assets/download-09Mhh4RE.js
+++ assets/download-09Mhh4RE.js
@@ -1,4 +1,4 @@
-import{g as kn,R as Wt,r as ht,j as v,D as An,d as On}from"./index-DnpzS7qT.js";/*!
+import{g as kn,R as Wt,r as ht,j as v,D as An,d as On}from"./index-BiWN2cfD.js";/*!
  * Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
